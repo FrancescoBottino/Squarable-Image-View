3c88146627359c76f4c0dfed0044e75e090b5901
Francesco Bottino <francesco.bottino96@gmail.com>
2021-03-10 12:02:06 +0100

    initial commit

---
+++ SquarableImageView/src/main/java/it/francescobottino/android/squarableimageview/SquarableImageView.kt
@@ -0,0 +1,45 @@
+package it.francescobottino.android.squarableimageview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class SquarableImageView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+): AppCompatImageView(context, attrs, defStyleAttr) {
+
+    enum class SquaringType {
+        NONE, WIDTH, HEIGHT
+    }
+
+    lateinit var squareOn: SquaringType
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.SquarableImageView, defStyleAttr, 0).apply {
+
+            try {
+                squareOn = SquaringType.values()[getInt(R.styleable.SquarableImageView_squareOn, 0)]
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        val height = measuredHeight
+
+        val chosenSide = when(squareOn) {
+            SquaringType.NONE -> return
+            SquaringType.HEIGHT -> height
+            SquaringType.WIDTH -> width
+        }
+
+        if(width != height) {
+            setMeasuredDimension(chosenSide, chosenSide)
+        }
+    }
+}
